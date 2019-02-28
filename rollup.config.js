d536b9adcfce2e640dde16002f25c679cfa06908
Eric Hough <eric@tubepress.com>
2019-02-27 22:31:47 -0800

    bump to 2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,72 +1,5 @@
-import babel            from 'rollup-plugin-babel';
-import commonjs         from 'rollup-plugin-commonjs';
-import replace          from 'rollup-plugin-replace';
-import resolve          from 'rollup-plugin-node-resolve';
-import { terser }       from 'rollup-plugin-terser';
-import visualizer       from 'rollup-plugin-visualizer';
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-import pkg              from './package.json';
-
-const isProduction = process.env.NODE_ENV === 'production',
-
-  isProfiling = process.env.NODE_ENV === 'profile',
-
-  replacements = () => {
-
-    const toStringify = { 'process.env.NODE_ENV' : isProduction ? 'production' : 'development' },
-      raw = { 'PRODUCTION' : isProduction };
-
-    Object.keys(toStringify).map((key) => {
-      toStringify[key] = JSON.stringify(toStringify[key]);
-    });
-
-    return {
-      ...toStringify,
-      ...raw,
-      delimiters: ['', ''],
-    };
-  },
-
-  buildPlugins = () => {
-
-    const plugins = [
-      resolve({ browser: true }),
-      commonjs(),
-      replace(replacements()),
-      replace({
-        include : 'node_modules/uuid/index.js',
-        'uuid.v1' : '//',
-      }),
-      babel({
-        babelrc: false,
-        runtimeHelpers: true,
-        presets: [
-          [
-            '@babel/env', {
-              modules : false,
-              forceAllTransforms: isProduction,
-            },
-          ],
-        ],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-        ],
-      }),
-      sizeSnapshot(),
-    ];
-
-    if (isProduction) {
-
-      plugins.push(terser());
-    }
-
-    if (isProfiling) {
-
-      plugins.push(visualizer());
-    }
-
-    return plugins;
-  };
+import babel from 'rollup-plugin-babel';
+import pkg   from './package.json';
 
 export default {
   input: 'src/index.js',
@@ -74,7 +7,20 @@ export default {
     file: 'es/index.js',
     format: 'es',
   },
-  plugins: buildPlugins(),
+  plugins: [
+    babel({
+      babelrc: false,
+      runtimeHelpers: true,
+      presets: [
+        [
+          '@babel/env', { modules : false },
+        ],
+      ],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+      ],
+    }),
+  ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
